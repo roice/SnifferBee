43d55482b9c5e5a62e3df32d61ff579945de4f0c
roice <roice@localhost.localdomain>
2016-04-29 12:11:53 +0800

    add netcard selection in UI

--- gs/GSRAO/src/ui/UI.cxx
+++ gs/GSRAO/src/ui/UI.cxx
@@ -20,26 +20,25 @@
 #include <FL/Fl_PNG_Image.H>
 #include <FL/Fl_Value_Input.H>
 #include <FL/Fl_Value_Slider.H>
+#include <FL/Fl_Choice.H>
 /* OpenGL */
 #include <FL/Fl_Gl_Window.H>
 #include <FL/gl.h>
 #include <FL/glut.H>
-/* RAOS */
+/* GSRAO */
 #include "ui/UI.h"
 #include "ui/icons/icons.h" // pixmap icons used in Tool bar
 #include "ui/View.h" // 3D RAO view
 #include "GSRAO_Config.h"
+/* Linux Network */
+#include <ifaddrs.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 
 /*------- Configuration Dialog -------*/
 struct ConfigDlg_widgets { // for parameter saving
-    // arena widith/length/height
-    Fl_Value_Input *arena_w;
-    Fl_Value_Input *arena_l;
-    Fl_Value_Input *arena_h;
-    // source position xyz
-    Fl_Value_Slider *source_x;
-    Fl_Value_Slider *source_y;
-    Fl_Value_Slider *source_z;
+    // network interface for receiving multicast info from Motive software (Opti-Track)
+    Fl_Choice *netcard;
 };
 class ConfigDlg : public Fl_Window
 {
@@ -51,7 +50,6 @@ private:
     // callback funcs
     static void cb_dlg(Fl_Widget*, void*);
     static void cb_switch_tabs(Fl_Widget*, void*);
-    static void cb_change_src_pos_bounds(Fl_Widget*, void*);
     // function to save current value of widgets to runtime configs
     static void save_value_to_configs(ConfigDlg_widgets*);
     // function to get runtime configs to set value of widgets
@@ -61,7 +59,7 @@ private:
 void ConfigDlg::cb_dlg(Fl_Widget* w, void* data) {
     if (Fl::event() == FL_CLOSE) {
         struct ConfigDlg_widgets *ws = (struct ConfigDlg_widgets*)data;
-        // save widget values to RAOS runtime configs when closing the dialog window
+        // save widget values to GSRAO runtime configs when closing the dialog window
         save_value_to_configs(ws);
         ((Fl_Window*)w)->hide();
     }
@@ -74,40 +72,12 @@ void ConfigDlg::cb_switch_tabs(Fl_Widget *w, void *data)
     tabs->selection_color( (tabs->value())->color() );
 }
 
-// change source position bounds according to arena size
-void ConfigDlg::cb_change_src_pos_bounds(Fl_Widget* arena_wlh, void* src_xyz) {
-    ((Fl_Value_Slider*)src_xyz)->minimum(-((Fl_Valuator*)arena_wlh)->value()/2.0);
-    ((Fl_Value_Slider*)src_xyz)->maximum(((Fl_Valuator*)arena_wlh)->value()/2.0);
-}
-
 void ConfigDlg::save_value_to_configs(ConfigDlg_widgets* ws) {
     GSRAO_Config_t* configs = GSRAO_Config_get_configs(); // get runtime configs
-    // save arena size
-    configs->arena.w = ws->arena_w->value();
-    configs->arena.l = ws->arena_l->value();
-    configs->arena.h = ws->arena_h->value();
-    // save source pos
-    configs->source.x = ws->source_x->value();
-    configs->source.y = ws->source_y->value();
-    configs->source.z = ws->source_z->value();
 }
 
 void ConfigDlg::set_value_from_configs(ConfigDlg_widgets* ws) {
     GSRAO_Config_t* configs = GSRAO_Config_get_configs(); // get runtime configs
-    // set arena size
-    ws->arena_w->value(configs->arena.w);
-    ws->arena_l->value(configs->arena.l);
-    ws->arena_h->value(configs->arena.h);
-    // set source pos & maximum
-    ws->source_x->value(configs->source.x);
-    ws->source_y->value(configs->source.y);
-    ws->source_z->value(configs->source.z);
-    ws->source_x->minimum(-configs->arena.w/2.0);
-    ws->source_y->minimum(-configs->arena.l/2.0);
-    ws->source_z->minimum(-configs->arena.h/2.0);
-    ws->source_x->maximum(configs->arena.w/2.0);
-    ws->source_y->maximum(configs->arena.l/2.0);
-    ws->source_z->maximum(configs->arena.h/2.0);
 }
 
 ConfigDlg::ConfigDlg(int xpos, int ypos, int width, int height, 
@@ -122,52 +92,56 @@ ConfigDlg::ConfigDlg(int xpos, int ypos, int width, int height,
     Fl_Tabs *tabs = new Fl_Tabs(t_x, t_y, t_w, t_h);
     {
         tabs->callback(cb_switch_tabs); // callback func when switch tabs
-        // Tab Scenario
-        Fl_Group *scenario = new Fl_Group(t_x,t_y+25,t_w,t_h-25,"Scenario");
+        // Tab Link
+        Fl_Group *link = new Fl_Group(t_x,t_y+25,t_w,t_h-25,"Link");
         {
             // color of this tab
-            scenario->color(0xe8e8e800); // light milk tea
-            scenario->selection_color(0xe8e8e800); // light milk tea
-            // Arena
-            Fl_Box *arena = new Fl_Box(t_x+10, t_y+25+10, 180, 130,"Arena");
-            arena->box(FL_PLASTIC_UP_FRAME);
-            arena->labelsize(16);
-            arena->labelfont(FL_COURIER_BOLD_ITALIC);
-            arena->align(Fl_Align(FL_ALIGN_TOP|FL_ALIGN_INSIDE));
-            // Arena width/length/height
-            dlg_w.arena_w = new Fl_Value_Input(t_x+10+60,t_y+25+10+30,80,25,"Width");
-            dlg_w.arena_l = new Fl_Value_Input(t_x+10+60,t_y+25+10+60,80,25,"Length");
-            dlg_w.arena_h = new Fl_Value_Input(t_x+10+60,t_y+25+10+90,80,25,"Height");
-            new Fl_Box(t_x+10+60+80,t_y+25+10+30, 20, 25, "m");
-            new Fl_Box(t_x+10+60+80,t_y+25+10+60, 20, 25, "m");
-            new Fl_Box(t_x+10+60+80,t_y+25+10+90, 20, 25, "m");
-            // Source
-            Fl_Box *source = new Fl_Box(t_x+10+190, t_y+25+10, 180, 130,"Source Position");
-            source->box(FL_PLASTIC_UP_FRAME);
-            source->labelsize(16);
-            source->labelfont(FL_COURIER_BOLD_ITALIC);
-            source->align(Fl_Align(FL_ALIGN_TOP|FL_ALIGN_INSIDE));
-            // Source pos
-            dlg_w.source_x = new Fl_Value_Slider(t_x+10+210,t_y+25+10+30,140,25,"X");
-            dlg_w.source_y = new Fl_Value_Slider(t_x+10+210,t_y+25+10+60,140,25,"Y");
-            dlg_w.source_z = new Fl_Value_Slider(t_x+10+210,t_y+25+10+90,140,25,"Z");
-            dlg_w.source_x->labelsize(16);
-            dlg_w.source_y->labelsize(16);
-            dlg_w.source_z->labelsize(16);
-            dlg_w.source_x->type(FL_HOR_NICE_SLIDER);
-            dlg_w.source_y->type(FL_HOR_NICE_SLIDER);
-            dlg_w.source_z->type(FL_HOR_NICE_SLIDER);
-            dlg_w.source_x->align(Fl_Align(FL_ALIGN_LEFT));
-            dlg_w.source_y->align(Fl_Align(FL_ALIGN_LEFT));
-            dlg_w.source_z->align(Fl_Align(FL_ALIGN_LEFT));
-            new Fl_Box(t_x+10+210+140,t_y+25+10+30, 20, 25, "m");
-            new Fl_Box(t_x+10+210+140,t_y+25+10+60, 20, 25, "m");
-            new Fl_Box(t_x+10+210+140,t_y+25+10+90, 20, 25, "m");
-            dlg_w.arena_w->callback(cb_change_src_pos_bounds, (void*)dlg_w.source_x);
-            dlg_w.arena_l->callback(cb_change_src_pos_bounds, (void*)dlg_w.source_y);
-            dlg_w.arena_h->callback(cb_change_src_pos_bounds, (void*)dlg_w.source_z);
+            link->color(0xe8e8e800); // light milk tea
+            link->selection_color(0xe8e8e800); // light milk tea
+            // Motion capture settings
+            Fl_Box *mocap = new Fl_Box(t_x+10, t_y+25+10, 180, 130,"Mocap");
+            mocap->box(FL_PLASTIC_UP_FRAME);
+            mocap->labelsize(16);
+            mocap->labelfont(FL_COURIER_BOLD_ITALIC);
+            mocap->align(Fl_Align(FL_ALIGN_TOP|FL_ALIGN_INSIDE));
+            //   Select network interface receiving the multicast info of Motion Capture System
+            dlg_w.netcard = new Fl_Choice(t_x+10+60, t_y+25+10+30, 80, 25, "Netcard");
+            //     get all network interfaces for choosing
+            struct ifaddrs* ifAddrStruct = NULL;
+            struct ifaddrs* ifa = NULL;
+            void* tmpAddrPtr = NULL;
+            char ncName[100];
+            getifaddrs(&ifAddrStruct);
+            for (ifa = ifAddrStruct; ifa != NULL; ifa = ifa->ifa_next) {
+                if (!ifa->ifa_addr) {
+                continue;
+                }
+                if (ifa->ifa_addr->sa_family == AF_INET) { // check it is IP4
+                    // is a valid IP4 Address
+                    tmpAddrPtr=&((struct sockaddr_in *)ifa->ifa_addr)->sin_addr;
+                    char addressBuffer[INET_ADDRSTRLEN];
+                    inet_ntop(AF_INET, tmpAddrPtr, addressBuffer, INET_ADDRSTRLEN);
+                    // add this net interface to choice list
+                    snprintf(ncName, 100, "%s %s IPv4", ifa->ifa_name, addressBuffer);
+                    dlg_w.netcard->add(ncName);
+                } else if (ifa->ifa_addr->sa_family == AF_INET6) { // check it is IP6
+                    // is a valid IP6 Address
+                    tmpAddrPtr=&((struct sockaddr_in6 *)ifa->ifa_addr)->sin6_addr;
+                    char addressBuffer[INET6_ADDRSTRLEN];
+                    inet_ntop(AF_INET6, tmpAddrPtr, addressBuffer, INET6_ADDRSTRLEN);
+                    // add this net interface to choice list
+                    snprintf(ncName, 100, "%s %s IPv6", ifa->ifa_name, addressBuffer);
+                    dlg_w.netcard->add(ncName);
+                } 
+            }
+            if (ifAddrStruct!=NULL) freeifaddrs(ifAddrStruct);
+            //    default choice
+            //dlg_w.netcard->callback(cb);
+            dlg_w.netcard->value(3);
+            dlg_w.netcard->tooltip("Select which network interface receives multicast info from Motive software");
+            
         }
-        scenario->end();
+        link->end();
         // Tab Flow
         Fl_Group *flow = new Fl_Group(t_x,t_y+25,t_w,t_h-25,"Flow");
         {
