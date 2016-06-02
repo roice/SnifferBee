5d8f7fc4f78a18b24ec826f9306fbb8b2685e6c3
roice <roice@localhost.localdomain>
2016-06-02 14:41:33 +0800

    add debug definition to make debuging easier

--- gs/GSRAO/src/robot/robot.cxx
+++ gs/GSRAO/src/robot/robot.cxx
@@ -15,14 +15,17 @@ static Robot_Ref_State_t robot_ref_state[4]; // 4 robots max
 
 std::vector<Robot_Record_t> robot_record[4]; // 4 robots max
 
+//#define DEBUG_HANDHELD_DEVICE
+
 /* robot init */
 bool robot_init(void)
 {
     if (!microbee_state_init())
         return false;
-
+#ifndef DEBUG_HANDHELD_DEVICE
     if (!microbee_control_init())
         return false;
+#endif
 
     // prepare for the robot record
     for (int i = 0; i < 4; i++) // 4 robots max
@@ -33,7 +36,9 @@ bool robot_init(void)
 
 void robot_shutdown(void)
 {
+#ifndef DEBUG_HANDHELD_DEVICE
     microbee_control_close();
+#endif
     microbee_state_close();
 }
 
