2c10755b146d0938486f88e6b3fc91dab591285d
roice <xingqing_lhtv@qq.com>
2016-09-12 10:19:47 +0800

    leso_test

--- gs/GSRAO/scripts/leso_test.py
+++ gs/GSRAO/scripts/leso_test.py
@@ -3,24 +3,43 @@ import matplotlib.pyplot as plt
 
 dt = 0.01   # s
 m = 1.0     # kg
-u = np.random.rand(10000)  # N
-temp_y = 0.0
-temp_vel = 0.0
+noise = (np.random.rand(10000) - 0.5)*0.2
+disturb = np.random.rand(10000) - 0.5  # N
+#disturb = np.ones(10000)
+
+temp_u = 0
+temp_vel = 0
+temp_y = 0
 y = []
-for i in range(len(u)):
-    temp_vel += u[i]/m*dt
+u = []
+kp_pos = 1.0
+kp_vel = 100.0
+kd_vel = 100.0
+ki_vel = 2.1
+error = 0
+d_error = 0
+i_error = 0
+for i in range(len(disturb)):
+    temp_vel += (noise[i]+disturb[i]+temp_u)/m*dt
+    temp_acc = -(noise[i]+disturb[i]+temp_u)/m*dt
     temp_y += temp_vel*dt
+    dest_vel = (0 - temp_y)*kp_pos
+    error = dest_vel - temp_vel
+    d_error = -temp_acc
+    i_error += error
+    temp_u = (error*kp_vel + d_error*kd_vel + i_error*ki_vel)*dt # ref = 0
     y.append(temp_y)
+    u.append(temp_u)
 
 # leso
-w0 = 10.0
+w0 = 100.0
 temp_z1 = 0.
 temp_z2 = 0.
 temp_z3 = 0.
 z1 = []
 z2 = []
 z3 = []
-for i in range(len(u)):
+for i in range(len(y)):
     leso_err = y[i] - temp_z1
     temp_z1 += dt*(temp_z2 + 3*w0*leso_err)
     temp_z2 += dt*(temp_z3 + 3*pow(w0, 2)*leso_err + u[i])
@@ -29,11 +48,14 @@ for i in range(len(u)):
     z2.append(temp_z2)
     z3.append(temp_z3)
 
-fig = plt.figure(figsize=(8,6))
-ax = fig.add_subplot(111)
+fig, axes = plt.subplots(nrows=3, figsize=(10, 10))
 
-ax.plot(y, color = 'blue')
-ax.plot(z1, color = 'green')
-ax.plot(z3, color = 'red')
+axes[0].plot(y, color = 'blue')
+axes[0].plot(z1, color = 'green')
+
+axes[1].plot(disturb, color = 'red')
+axes[1].plot(z3, color = 'green')
+
+axes[2].plot(u, color = 'blue')
 
 plt.show()
